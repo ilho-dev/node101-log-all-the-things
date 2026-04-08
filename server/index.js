e4c6595ec20ef2c3fec177c03f041bbf9c096c1b
Ilho Choi <ilho-dev@users.noreply.github.com>
2026-04-08 10:01:23 -0700

    fix: dynamic port for Render deployment

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const server = require('./app');
 
-server.listen(3000, () => {
-  console.log('server is listening on 3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`server is listening on ${PORT}`);
 })
